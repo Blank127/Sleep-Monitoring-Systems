b85f1eb5a6fca5cbe6d87b1fc8fae3ff2459edda
Aldrich Dias <93847664+Blank127@users.noreply.github.com>
2026-04-27 15:05:28 -0600

    Backend created

---
+++ SleepMonitor/SleepMonitor.BLE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SleepMonitor.BLE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
